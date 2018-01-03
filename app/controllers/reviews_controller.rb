1d2bb464e796cccd785c1a729804107e820f5ac7
shalom peace <shalompc74@yahoo.com>
2018-01-02 21:17:33 -0800

    set up link and routing for Review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,7 +20,7 @@ class ReviewsController < ApplicationController
     @review.restaurant_id = @restaurant.id
     respond_to do |format|
       if @review.save
-        format.html { redirect_to root_path, notice: 'Review was successfully created.' }
+        format.html { redirect_to restaurants_path(@restaurant), notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
