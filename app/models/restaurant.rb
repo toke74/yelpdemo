bac0ff3c6403eca38aecd75e0824b4e1bd5bd89e
shalom peace <shalompc74@yahoo.com>
2018-01-07 16:52:08 -0800

    Add data validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,20 @@
 class Restaurant < ApplicationRecord
 	mount_uploader :image, ImageUploader
 
-	 has_many :reviews
+	has_many :reviews
+
+	validates :name, :address, :phone, :website, :image, presence: true
+
+	validates :phone, format: { 
+		with: /\A\(\d\d\d\) \d\d\d\-\d\d\d\d\z/,
+    	message: "must be format of (123) 321-7654" }
+
+	validates :website, format: { 
+		with: /\Ahttps?:\/\/w{3}.*\z/,
+    	message: "must start with https:// or http://" }
+
+	validates :address, format: { 
+		with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    	message: "must be in the format 350 Fifth Avenue, New York, NY 10118" }
+    
 end
